82fd25a12bf2746f65ed8802ab0a5206905c1320
PratikTiruwa <userpc0498@gmail.com>
2024-10-24 23:18:55 +0530

    Update fibonacci.sol

--- Practical 7/nth term of Fibonacci Series/fibonacci.sol
+++ Practical 7/nth term of Fibonacci Series/fibonacci.sol
@@ -10,12 +10,13 @@ contract fibonacci {
 
          uint256 n2 = 1; 
 
-         uint n3;
+         uint256 n3;
 
          for (uint256 i = 3; i <= a; i++) {
 
             n3 = n1 + n2;
             n1 = n2;
+            n2 = n3; 
         }
 
         return n3;
